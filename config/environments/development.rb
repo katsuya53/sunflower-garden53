d956c6990a0f79ed13c4de802a275a8fa049ddc5
katsuya53 <redstar53hk@gmail.com>
2024-04-02 23:08:01 +0900

    環境変数の修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:password],
     authentication:       'login',
     enable_starttls_auto: true
   }
