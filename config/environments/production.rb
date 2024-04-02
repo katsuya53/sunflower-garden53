cb48abcf6cf879c15519f11fdfffa036fb57c8c1
katsuya53 <redstar53hk@gmail.com>
2024-04-02 23:39:00 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            Rails.application.credentials.gmail[:user_name],
-    password:             Rails.application.credentials.gmail[:password],
+    user_name:            "sunflower53garden",
+    password:             "sun08440844",
     authentication:       'login'
   }
 
