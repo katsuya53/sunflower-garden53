d0481fb831be77a8c8977474d6b69500011abf73
katsuya53 <redstar53hk@gmail.com>
2024-04-03 22:14:27 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
     user_name:            Rails.application.credentials.gmail[:user_name],
-    password:             Rails.application.credentials.gmail[:password],
+    password:             "svkj llwi djuc dslp",
     authentication:       'login',
     enable_starttls_auto: true
   }
