1162deadf0caac449892c4ef75748589d8a5f399
katsuya53 <redstar53hk@gmail.com>
2024-04-04 22:43:17 +0900

    環境変数の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            Rails.application.credentials.gmail[:user_name],
-    password:             Rails.application.credentials.gmail[:password],
+    user_name:            "sunflower53garden@gmail.com",
+    password:             "jysv piex zmsc uubd",
     authentication:       'login',
     enable_starttls_auto: true
   }
