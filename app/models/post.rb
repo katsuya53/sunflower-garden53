786ac7950dfc8e44630f8770536934f34b9075da
katsuya53 <redstar53hk@gmail.com>
2024-04-14 19:51:03 +0900

    投稿削除時エラーの紐付け修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments
-  has_many :post_tag_relations
+  has_many :comments, dependent: :destroy
+  has_many :post_tag_relations, dependent: :destroy
   has_many :tags, through: :post_tag_relations
   
 
