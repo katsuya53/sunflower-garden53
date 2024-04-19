638f99b006be555b8a5599ce8acb59232d017cea
katsuya53 <redstar53hk@gmail.com>
2024-04-20 00:39:34 +0900

    デプロイ不具合修正

--- db/migrate/20240418145940_rename_iine_count_to_likes_count_in_posts.rb
+++ db/migrate/20240418145940_rename_iine_count_to_likes_count_in_posts.rb
@@ -1,8 +1,5 @@
 class RenameIineCountToLikesCountInPosts < ActiveRecord::Migration[6.0]
   def change
-    # iine_countカラムが存在する場合のみ、カラム名を変更する
-    if column_exists?(:posts, :iine_count)
-      rename_column :posts, :iine_count, :likes_count
-    end
+    rename_column :posts, :iine_count, :likes_count
   end
 end
