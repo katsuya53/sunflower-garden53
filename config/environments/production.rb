743cd75603471c4faba217d08d3a417cf62759cc
katsuya53 <redstar53hk@gmail.com>
2024-04-20 10:56:38 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   
-
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
