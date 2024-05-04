2e3bc70ba2ff59c8bb305adeb7e7c6796edd906d
katsuya53 <redstar53hk@gmail.com>
2024-05-04 09:03:07 +0900

    Update 20240503102526_create_flowers.rb

--- db/migrate/20240503102526_create_flowers.rb
+++ db/migrate/20240503102526_create_flowers.rb
@@ -1,7 +1,6 @@
 class CreateFlowers < ActiveRecord::Migration[7.0]
   def change
-    create_table :flowers do |t|
-
+    create_table :flowers, if_not_exists: true do |t|
       t.timestamps
     end
   end
