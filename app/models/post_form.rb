3ee29e063671cf9907862398f64a4eb6911e6d38
katsuya53 <redstar53hk@gmail.com>
2024-05-18 14:39:47 +0900

    新規投稿時のエラーメッセージ日本語化

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -8,47 +8,48 @@ class PostForm
   )
 
   with_options presence: true do
-    validates :post_title, presence: true
-    validates :post_text, presence: true
-    validates :image, presence: true
+    validates :post_title
+    validates :post_text
+    validates :image
   end
-  
+
   def save
     post = Post.new(post_title: post_title, post_text: post_text, image: image, user_id: user_id)
-    
+
     if post.save
       save_tags(post)
-      return true
+      true
     else
-      post.errors.full_messages.each do |message|
-        errors.add(:base, message)
-      end
-      return false
+      merge_errors(post)
+      false
     end
   end
-  
+
   def update(params, post)
     post.assign_attributes(params.except(:tag_name, :tag_name2, :tag_name3))
     if post.save
       post.tags.destroy_all # 既存のタグを削除
       save_tags(post)
-      return true
+      true
     else
-      post.errors.full_messages.each do |message|
-        errors.add(:base, message)
-      end
-      return false
+      merge_errors(post)
+      false
     end
   end
-  
-  
+
   private
-  
+
   def save_tags(post)
     [tag_name, tag_name2, tag_name3].compact.each do |tag_name|
       tag = Tag.where(tag_name: tag_name).first_or_initialize
       tag.save
       PostTagRelation.create(post_id: post.id, tag_id: tag.id)
+    end
+  end
+
+  def merge_errors(post)
+    post.errors.each do |attribute, message|
+      self.errors.add(attribute, message)
     end
   end
 end
