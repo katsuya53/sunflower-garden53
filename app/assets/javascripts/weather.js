e8a1bdc1e45dda55f1f9b0619ae87d0b97aee6fa
katsuya53 <redstar53hk@gmail.com>
2024-06-02 15:26:23 +0900

    Update weather.js

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,5 +1,3 @@
-// app/assets/javascripts/weather.js
-
 function get_weather_japanese(weather_main) {
   switch (weather_main) {
     case "Clouds":
@@ -14,6 +12,36 @@ function get_weather_japanese(weather_main) {
       return "雪";
     case "Clear":
       return "晴れ";
+    case "clear sky":
+      return "快晴";
+    case "few clouds":
+      return "くもり（雲少なめ）";
+    case "scattered clouds":
+      return "くもり（雲ふつう）";
+    case "broken clouds":
+      return "くもり（雲多め）";
+    case "overcast clouds":
+      return "くもり（雲多め）";
+    case "light intensity shower rain":
+      return "小雨のにわか雨";
+    case "shower rain":
+      return "にわか雨";
+    case "heavy intensity shower rain":
+      return "大雨のにわか雨";
+    case "light rain":
+      return "小雨";
+    case "moderate rain":
+      return "雨";
+    case "heavy intensity rain":
+      return "大雨";
+    case "very heavy rain":
+      return "激しい大雨";
+    case "thunderstorm":
+      return "雷雨";
+    case "mist":
+      return "靄";
+    case "tornado":
+      return "強風";
     default:
       return weather_main;
   }
