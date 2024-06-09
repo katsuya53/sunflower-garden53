67742878a261b532cb4a7382b9b4e6951e392b23
katsuya53 <redstar53hk@gmail.com>
2024-06-09 14:34:39 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example.com', password: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include("ニックネーム を入力してください")
+    end
+
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include("メールアドレス を入力してください")
+    end
+  end
 end
