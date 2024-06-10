604531acd782218b3f779f08aae3a0e55366ab8c
katsuya53 <redstar53hk@gmail.com>
2024-06-10 19:13:27 +0900

    blogテストコード完成

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :blog do
+    blog_title { Faker::Lorem.sentence }
+    blog_text { Faker::Lorem.sentence }
+    association :user
     
   end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -6,30 +6,19 @@ RSpec.describe Blog, type: :model do
   end
 
   describe '投稿の保存' do
-    context '投稿が投稿できる場合' do
-      it '画像とテキストを投稿できる' do
-        expect(@blog).to be_valid
-      end
-    end
 
     context '投稿できない場合' do
       it 'タイトルが空では投稿できない' do
         @blog.blog_title = ''
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("blog title を入力してください")
+        expect(@blog.errors.full_messages).to include("Blog title を入力してください")
       end
 
       it '投稿文が空では投稿できない' do
         @blog.blog_text = ''
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("blog text を入力してください")
+        expect(@blog.errors.full_messages).to include("Blog text を入力してください")
       end 
-
-      it '画像が空では投稿できない' do
-        @blog.image = nil
-        @blog.valid?
-        expect(@blog.errors.full_messages).to include("Image を入力してください")
-      end  
 
       it 'ユーザーが紐付いていなければ投稿できない' do
         @blog.user = nil
