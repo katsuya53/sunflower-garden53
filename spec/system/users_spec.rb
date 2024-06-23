2a295362f403f88948452fcf05b4b5beb218ed3b
katsuya53 <redstar53hk@gmail.com>
2024-06-23 22:53:08 +0900

    結合テスト user-追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,10 +7,22 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができるとき' do 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password-confirmation', with: @user.password_confirmation
+      select '京都府', from: 'user[prefecture_id]'
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count }.by(1)
       # トップページへ遷移することを確認する
       # カーソルを合わせるとログアウトボタンが表示されることを確認する
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
