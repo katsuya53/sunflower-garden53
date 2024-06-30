5adbb3b21a5a124d8c842e777ad3fd77ec100f45
katsuya53 <redstar53hk@gmail.com>
2024-06-30 23:31:54 +0900

    投稿のテストコード実装開始

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :system do
+RSpec.describe 'ツイート投稿', type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @post_text = Faker::Lorem.sentence
+    @post_image = Faker::Lorem.sentence
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ツイート投稿ができるとき'do
+    it 'ログインしたユーザーは新規投稿できる' do
+      # ログインする
+      # 新規投稿ページへのボタンがあることを確認する
+      # 投稿ページに移動する
+      # フォームに情報を入力する
+      # 送信するとTweetモデルのカウントが1上がることを確認する
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+    end
+  end
+  context 'ツイート投稿ができないとき'do
+    it 'ログインしていないと新規投稿ページに遷移できない' do
+      # トップページに遷移する
+      # 新規投稿ページへのボタンがないことを確認する
+    end
+  end
 end
