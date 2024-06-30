4cf4f7a8ff90aef4dcfebbbc1e610a8ac2dac89d
katsuya53 <redstar53hk@gmail.com>
2024-06-30 23:57:54 +0900

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,11 +10,9 @@ describe PostsController, type: :request do
       get root_path
       expect(response.status).to eq 200
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのテキストが存在する' do 
       get root_path
       expect(response.body).to include(@post.post_text)
-    end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
     end
